41affbbff7b55fdc36939d0752f61905efb351a2
QuanNguyen <quan.nguyen.2629@gmail.com>
2025-01-08 14:31:58 +0700

    add ipa

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+// Static file hosting directory
+app.use(express.static(path.join(__dirname, "public")));
+
+// Route for the manifest.plist
+app.get("/manifest.plist", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "manifest.plist"));
+});
+
+// Route for the .ipa file
+app.get("/app.ipa", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "plugilo.ipa"));
+});
+
+// Default route to show information
+app.get("/", (req, res) => {
+  res.send(`
+        <h1>iOS App Hosting Server</h1>
+        <p>To download the app, use the following link:</p>
+        <p><a href="itms-services://?action=download-manifest&url=https://yourserver.com/manifest.plist">
+            Install App
+        </a></p>
+    `);
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
